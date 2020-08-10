f315d3d83c8f44fc86379d696aec55c45663ba5f
Anthony Mottot <anthony.mottot@monster.com>
2020-08-10 11:51:30 +0200

    Preparation GitDemo

---
+++ src/main/java/com/monster/git/demo/App.java
@@ -0,0 +1,13 @@
+package com.monster.git.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
